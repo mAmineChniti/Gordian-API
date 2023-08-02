24663408d4f2ba0657862776c62e15d059dd514a
mAmineChniti <emin.chniti@esprit.tn>
2023-08-02 14:10:13 +0100

    manually added status codes

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -13,15 +13,15 @@ async def redirect_to_docs():
     return RedirectResponse(url='/docs')
 
 # API Status check
-@UsersRouter.get("/status", include_in_schema=False)
+@UsersRouter.get("/status", include_in_schema=False, status_code=200)
 async def status():
     return {"status": "ok"}
 
-@UsersRouter.post("/register", status_code=status.HTTP_201_CREATED)
+@UsersRouter.post("/register", status_code=201)
 async def register(user: User):
     # Check if the username already exists
     if await users_collection.find_one({"username": user.username}):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exists")
+        raise HTTPException(status_code=400, detail="Username already exists")
 
     # Hash the password with a stronger encoding
     hashed_password = hashpw(user.password.encode("utf-8"), gensalt(rounds=12, prefix=b"2b"))
@@ -39,13 +39,13 @@ async def register(user: User):
 
     return {"message": "User registered successfully"}
 
-@UsersRouter.post("/login")
+@UsersRouter.post("/login", status_code=202)
 async def login(user: User):
     # Retrieve the user from the database
     stored_user = await users_collection.find_one({"username": user.username})
 
     # Check if the user exists and the password matches
     if not stored_user or not checkpw(user.password.encode("utf-8"), stored_user["password"].encode("utf-8")):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid username or password")
+        raise HTTPException(status_code=401, detail="Invalid username or password")
 
     return {"uuid": stored_user["_id"]}
